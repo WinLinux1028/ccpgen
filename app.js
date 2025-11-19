749decf71fa1a50840411d920ee2c050f74d73c8
mixivalo <32605294+mixivalo@users.noreply.github.com>
2025-11-19 09:04:57 +0900

    外交部新色対応

--- app.js
+++ app.js
@@ -187,6 +187,8 @@ const FLAG_DEFAULT_CODES = { flag1: 'CN', flag2: 'JP' };
 const BACKGROUND_TYPES = {
   FOREIGN_AFFAIRS: 'background.png',
   FOREIGN_AFFAIRS2: 'background1.png',
+  FOREIGN_AFFAIRS3: 'background1.1.png',
+  FOREIGN_AFFAIRS4: 'background1.2.png',
   DEFENSE: 'background2.png',
   MAO_NING: 'background3.png'
 };
@@ -195,6 +197,8 @@ function getFooterPrefixes() {
   return {
     FOREIGN_AFFAIRS: i18nextInstance ? i18nextInstance.t('footerForeignAffairs') : '中国外交部報道官',
     FOREIGN_AFFAIRS2: i18nextInstance ? i18nextInstance.t('footerForeignAffairs2') : '中国外交部報道官',
+    FOREIGN_AFFAIRS3: i18nextInstance ? i18nextInstance.t('footerForeignAffairs2') : '中国外交部報道官',
+    FOREIGN_AFFAIRS4: i18nextInstance ? i18nextInstance.t('footerForeignAffairs2') : '中国外交部報道官',
     DEFENSE: i18nextInstance ? i18nextInstance.t('footerDefense') : '中国国防部報道官'
   };
 }
